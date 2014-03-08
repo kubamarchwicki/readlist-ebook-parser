ee627a7cf5879cf765fca5088d7da2afbe01c4dc
kubamarchwicki <kuba@marchwicki.pl>
2014-03-08 22:54:35 +0100

    Allow after download modification: unfav item and archive item

--- readpick/readlists/main.py
+++ readpick/readlists/main.py
@@ -17,13 +17,15 @@ def run():
     parser.add_argument('-p', '--password', nargs='?', help='Pocket password', required=True)
     parser.add_argument('-c', '--count', nargs='?', help='Number of articles to download', default=10)
     parser.add_argument('-f', '--favourite', help='Download only favourite', action="store_true")
+    parser.add_argument('-mu', '--modify_unfav', help='Modify unfavourite after download', action="store_true")
+    parser.add_argument('-ma', '--modify_arch', help='Modify archive after download', action="store_true")
     parser.add_argument('-s', '--sort', nargs='?', help='Sorting order', default='newest')
 
     args = parser.parse_args()
     logger.debug('Command line params %s' % args)
     controller(args)
 
-#TODO: unfavourite and archive downloaded articles
+
 def controller(args):
     pocket = Pocket(username=args.username,
                     password=args.password,
@@ -31,7 +33,15 @@ def controller(args):
                     count=args.count)
     article_list = pocket.get_list()
 
-    print output_template % (date.today().strftime("%d/%m/%Y"), json.dumps(article_list, indent=16))
+    items = map(lambda x: x['item_id'], article_list)
+    if args.modify_unfav:
+        pocket.modify(items, 'unfavorite')
+
+    if args.modify_arch:
+        pocket.modify(items, 'archive')
+
+
+    print output_template % (date.today().strftime("%d/%m/%Y"), json.dumps(article_list, indent=8))
 
 
 output_template = """{
